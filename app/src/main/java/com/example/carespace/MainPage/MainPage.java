37e52a660c631b740f32180833b4593ad0353062
NEIL-smtg <72685169+NEIL-smtg@users.noreply.github.com>
2022-03-19 01:26:46 +0800

    added search hostpital/clinic nearby

--- app/src/main/java/com/example/carespace/MainPage/MainPage.java
+++ app/src/main/java/com/example/carespace/MainPage/MainPage.java
@@ -1,47 +1,319 @@
 package com.example.carespace.MainPage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.carespace.JsonParser;
 import com.example.carespace.R;
 import com.example.carespace.login.Login;
+import com.example.carespace.logout.LogOut;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.okhttp.HttpUrl;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainPage extends AppCompatActivity {
 
-    TextView email,password;
-    Button logout;
-    FirebaseAuth firebaseAuth;
+    Spinner spType;
+    Button btFind;
+    SupportMapFragment supportMapFragment;
+    GoogleMap map;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    double currentLat = 0, currentLong = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-        email = (TextView) findViewById(R.id.mainpage_email);
-        password = (TextView) findViewById(R.id.mainpage_password);
-        logout = (Button) findViewById(R.id.logoutbtn);
+        spType = (Spinner) findViewById(R.id.sp_type);
+        btFind = (Button) findViewById(R.id.bt_find);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        //initialize array of place type
+        String[] placeTypeList = new String[]{"hospital", "movie_theater", "clinic"};
+        //initialize array of place name
+        String[] placeNameList = new String[]{"Hospital", "Movie Theater", "Clinic"};
 
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        email.setText(user.getEmail());
-        password.setText(user.getDisplayName());
+        //set adapter on spinner
+        spType.setAdapter(new ArrayAdapter<>(MainPage.this, android.R.layout.simple_spinner_dropdown_item, placeNameList));
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        //initialize fused location provider client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        LocationSetting();
+        getCurrentLocation();
+
+        btFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainPage.this, Login.class));
-                finish();
+                //get selected position of spinner
+                int i = spType.getSelectedItemPosition();
+                //initialize url
+                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" + //url
+                        "?location=" + currentLat +","+ currentLong +    //longitude and latitude
+                        "&radius=5000" +        //radius
+                        "&types=" + placeTypeList[i] +
+                        "&sensor=true" +
+                        "&key=" + getResources().getString(R.string.google_map_key);
+
+                //execute place task method to download json data
+                new PlaceTask().execute(url);
             }
         });
 
+    }
+
+    private void LocationSetting() {
+        //check if user turn on location
+        if (!checkLocationEnabled()) {
+            //notify user
+            new AlertDialog.Builder(this)
+                    .setMessage(R.string.gps_network_not_enabled)
+                    .setPositiveButton(R.string.go_to_location_setting, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(MainPage.this, LogOut.class));
+                            finish();
+                        }
+                    })
+                    .show();
+
+        }
+    }
+
+
+    private boolean checkLocationEnabled()
+    {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean gps_enabled = false;
+
+        try {
+            gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }catch (Exception ex){
+            Toast.makeText(MainPage.this, ""+ex.getMessage(), Toast.LENGTH_SHORT).show();
+        };
+
+        return gps_enabled;
+    }
+
+
+    private void getCurrentLocation() {
+
+        //check permission
+        if (ActivityCompat.checkSelfPermission(MainPage.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //when permission granted
+            //initialize task location
+
+            Task<Location> task = fusedLocationProviderClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, new CancellationToken() {
+                @NonNull
+                @Override
+                public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
+                    return null;
+                }
+
+                @Override
+                public boolean isCancellationRequested() {
+                    return false;
+                }
+            });
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null)
+                    {
+                        //when location is not equal to null
+                        //get current latitude and longitude
+                        currentLat = location.getLatitude();
+                        currentLong = location.getLongitude();
+
+                        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(@NonNull GoogleMap googleMap) {
+                                //when map ready
+                                map = googleMap;
+                                //zoom current location on map
+                                map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLat,currentLong),10));
+                            }
+                        });
+                    }
+                }
+            });
+
+        } else {
+            //when permission denied
+            //request permission
+            ActivityCompat.requestPermissions(MainPage.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private class PlaceTask extends AsyncTask<String,Integer,String>
+    {
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            String data = null;
+
+            try {
+                //initialize data
+                data = downloadUrl(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            new ParserTask().execute(s);
+        }
+    }
+
+    private String downloadUrl(String string) throws IOException
+    {
+        //initialize url
+        URL url = new URL(string);
+        //initialize connection
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        //Connect connection
+        connection.connect();
+        //initialize input stream
+        InputStream stream = connection.getInputStream();
+        //initialize buffer reader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        //initialize string bulder
+        StringBuilder builder = new StringBuilder();
+        //initialize string var
+        String line = "";
+
+        while ((line= reader.readLine()) != null)
+        {
+            //append line
+            builder.append(line);
+        }
+
+        //get append data
+        String data = builder.toString();
+        //close reader
+        reader.close();
+
+        return data;
+    }
+
+    private class ParserTask extends AsyncTask<String,Integer, List<HashMap<String,String>>>
+    {
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... strings) {
+            //create json parser class
+            JsonParser jsonParser = new JsonParser();
+            //initialize hash map list
+            List<HashMap<String,String>> mapList = null;
+            JSONObject object = null;
+
+            try {
+                //initialize json object
+                object = new JSONObject(strings[0]);
+                //parse json object
+                mapList = jsonParser.parseResult(object);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+            //clear map
+            map.clear();
+
+            for (int i = 0; i < hashMaps.size(); i++)
+            {
+                HashMap<String,String> hashMapList = hashMaps.get(i);
+
+                //get long and latitude
+                double lat = Double.parseDouble(hashMapList.get("lat"));
+                double lng = Double.parseDouble(hashMapList.get("lng"));
+                String name = hashMapList.get("name");
+
+                LatLng latLng = new LatLng(lat,lng);
+
+                MarkerOptions options = new MarkerOptions();
+
+                options.position(latLng);
+
+                options.title(name);
+
+                //add marker on map
+                map.addMarker(options);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/carespace/logout/LogOut.java
@@ -0,0 +1,45 @@
+package com.example.carespace.logout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.carespace.MainPage.MainPage;
+import com.example.carespace.R;
+import com.example.carespace.login.Login;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogOut extends AppCompatActivity {
+    TextView email,password;
+    Button logout;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_out);
+
+        email = (TextView) findViewById(R.id.mainpage_email);
+        password = (TextView) findViewById(R.id.mainpage_password);
+        logout = (Button) findViewById(R.id.logoutbtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        email.setText(user.getEmail());
+        password.setText(user.getDisplayName());
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(LogOut.this, Login.class));
+                finish();
+            }
+        });
+    }
+}
